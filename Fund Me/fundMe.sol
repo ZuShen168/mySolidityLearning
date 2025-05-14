6cfde6db4b3f1d0652f7c8e5cf5d03a6b076e48d
ZuShen168 <ngzushen@gmail.com>
2025-05-14 22:04:45 +0800

    price converter library

--- Fund Me/fundMe.sol
+++ Fund Me/fundMe.sol
@@ -1,30 +1,19 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe{
     
+    using PriceConverter for uint256;
     uint256 public minimumUSD = 5e18;
 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
     function fund() public payable {
-
-        require(convert(msg.value) >= minimumUSD, "didn't send enough ETH");
+        require(msg.value.convert() >= minimumUSD, "didn't send enough ETH");
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
-    }
-
-    function getPrice() public view returns(uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int256 price, , , ) = priceFeed.latestRoundData();
-        return uint256(price)*1e10;
-    }
-
-    function convert(uint256 amount) public view returns (uint256){
-        uint256 price = getPrice();
-        return (price*amount)/1e18;
     }
 
 }
